b0cb8f9881d074a47f81e5ba2e9c43a1259fa573
Gustavo <guhzinho2016@gmai.com>
2026-05-22 11:49:39 -0300

    vamo

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,12 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: ['query', 'error', 'warn'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
